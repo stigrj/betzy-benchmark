5cccf57f23bbdcea8b25af4264b6364c370c5ef2
Stig Rune Jensen <stig.r.jensen@uit.no>
2020-05-29 12:57:48 +0200

    Put modules in separate file

--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash -l
 
-module --quiet purge
-module load CMake/3.12.1
-module load intel/2019b
-module load Python/3.7.4-GCCcore-8.3.0
-
 mrchem_version="v1.0.0-alpha1"
 mrchem_url="https://github.com/MRChemSoft/mrchem/archive/${mrchem_version}.tar.gz"
 
@@ -13,6 +8,7 @@ build_dir="mrchem_build"
 install_dir="mrchem_install"
 
 echo "-- Setting up environment"
+source modules.txt
 mkdir -p venv
 virtualenv venv
 source venv/bin/activate
